88184af9deace5d25fb004862e3ee560078d3a4b
ItsNiSantana <bianewlife@gmail.com>
2026-01-14 12:33:00 -0300

    1401202603

---
+++ variables.py
@@ -0,0 +1,9 @@
+int_value = 4
+print(int_value) #print the variable's value
+print(type(int_value)) #print the variable's type/class
+
+print()
+
+float_value = float(int_value)
+print(float_value)
+print(type(float_value))
