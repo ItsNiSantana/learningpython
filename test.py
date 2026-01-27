594a73fe9a00980a7b548e0d16b6e24a9cf89913
ItsNiSantana <bianewlife@gmail.com>
2026-01-27 15:30:31 -0300

    27012006

---
+++ test.py
@@ -0,0 +1,17 @@
+weight = 75
+
+# Write your if statement here:
+if (weight > 70):
+  fee = 100
+  print("The bag's weight is: " + str(weight) + " pounds.")
+  print("The fee for the bag is: " + str(fee) + " dollars.")
+
+elif (50 <= weight <= 70):
+  fee = 50
+  print("The bag's weight is: " + str(weight) + " 100 pounds.")
+  print("The fee for the bag is: " + str(fee) + " dollars.")
+
+else:
+  fee = 0
+  print("The bag's weight is: " + str(weight) + " 100 pounds.")
+  print("The fee for the bag is: " + str(fee) + " dollars.")
